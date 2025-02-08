444097c807ce44a0aa7d4f9b1d283563d7e4ff04
Daniel <daniel.byshovets@gmail.com>
2025-02-07 22:04:53 -0800

    Сommit 3 (22.01.2025): developed login and registration functions, improved styles, fixed products add feature, added clients and products (user's view)

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import hashlib
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clothes_shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+def compute_sha512_hash(input_string):
+    sha512_hash = hashlib.sha512()
+    sha512_hash.update(input_string.encode('utf-8'))
+    return sha512_hash.hexdigest()
 
 
 class Order(db.Model):
@@ -30,30 +37,87 @@ class Product(db.Model):
         return '<Product %r>' % self.id
 
 
-@app.route("/")
-def index():
-    return render_template("welcome_page.html")
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    surname = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(19), nullable=False)
+    cart = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return '<Client %r>' % self.id
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        hash_password = compute_sha512_hash(password)
+
+        clients = Client.query.all()
+        found_user = None
+        for i in clients:
+            if i.email == login:
+                if i.password == hash_password:
+                    found_user = i
+                    break
+
+        if found_user is None:
+            return redirect("/")
+        if found_user.email.endswith("admin"):
+            return render_template("welcome_page.html")
+        else:
+            products = Product.query.all()
+            return render_template("product_u.html", products=products, id=found_user.id)
+    else:
+        return render_template("login.html")
 
 
-@app.route("/clothes")
-def clothes():
+@app.route("/registration", methods=['POST', 'GET'])
+def show_registration():
+    if request.method == "POST":
+        name = request.form['name']
+        surname = request.form['surname']
+        phone = request.form['phone']
+        email = request.form['email']
+        password = request.form['password']
+
+        hash_password = compute_sha512_hash(password)
+
+        new_client = Client(name=name, surname=surname, phone=phone, email=email, password=str(hash_password), cart="")
+
+        try:
+            db.session.add(new_client)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Помилка при додаванні клієнта"
+    else:
+        return render_template("registration.html")
+
+
+@app.route("/clients")
+def show_clients():
+    clients = Client.query.all()
+    return render_template("clients.html", clients=clients)
+
+
+@app.route("/<int:id>/buy")
+def buy(id):
+    client = Client.query.get_or_404(id)
+
+
+@app.route("/products")
+def show_products():
     products = Product.query.all()
-    return render_template("clothes_a.html", products=products)
+    return render_template("product_a.html", products=products)
 
 
-@app.route("/orders")
-def show_orders():
-    orders = Order.query.all()
-    return render_template("orders.html", orders=orders)
-
-
-@app.route('/clothes/create', methods=['POST', 'GET'])
-def create_clothes():
+@app.route('/product/create', methods=['POST', 'GET'])
+def create_product():
     if request.method == "POST":
         title = request.form['title']
         price = request.form['price']
@@ -66,9 +130,15 @@ def create_clothes():
             db.session.commit()
             return redirect("/")
         except:
-            return "Помилка при додаванні статті"
+            return "Помилка при додаванні товару"
     else:
         return render_template("product_create.html")
+
+
+@app.route("/orders")
+def show_orders():
+    orders = Order.query.all()
+    return render_template("orders_a.html", orders=orders)
 
 
 @app.route("/orders/create", methods=['POST', 'GET'])
@@ -89,6 +159,11 @@ def create_order():
             return "Помилка при додаванні замовлення"
     else:
         return render_template("order_create.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
